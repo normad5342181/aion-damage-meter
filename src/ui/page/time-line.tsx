4996011fc4ddff91d37baccd6e4ddc1a032b75f9
lupeng <lupeng@kernelsoft.com>
2026-03-23 23:48:51 +0800

    feat: 时间选择完成

--- src/ui/page/time-line.tsx
+++ src/ui/page/time-line.tsx
@@ -1,8 +1,9 @@
-import { Button, Timeline, Tooltip } from "antd";
+import { Button, DatePicker, Timeline, Tooltip } from "antd";
 import { Tag } from "../../worker/read-log/types";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TimelineItemType } from "antd/es/timeline/Timeline";
 import { calcTimelineItemHeight } from "../util";
+import dayjs from "dayjs";
 
 interface IProps {
   startTime: string;
@@ -16,80 +17,81 @@ function TimeLineView({ startTime, endTime, tagList }: IProps) {
     new Date(endTime).getTime(),
   ]);
   const [marks, setMarks] = useState<TimelineItemType[]>([]);
+  const startTimeRef = useRef<number>(0);
+  const endTimeRef = useRef<number>(0);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const markRender = (title: number | string, content: string) => {
-    const titleView = (
-      <Tooltip title="设为开始">
-        <span
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            updateSlider(new Date(title).getTime());
-          }}
-        >
-          {new Date(title).toLocaleString()}
-        </span>
-      </Tooltip>
-    );
+  const updateSlider = (startDt?: number | string, endDt?: number | string) => {
+    const markRender = (title: number | string, content: string) => {
+      const titleView = (
+        <Tooltip title="设为开始">
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              updateSlider(new Date(title).getTime());
+            }}
+          >
+            {new Date(title).toLocaleString()}
+          </span>
+        </Tooltip>
+      );
 
-    const contentView = (
-      <Tooltip title="设为结束">
-        <span
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            updateSlider(undefined, new Date(title).getTime());
-          }}
-        >
-          {content}
-        </span>
-      </Tooltip>
-    );
+      const contentView = (
+        <Tooltip title="设为结束">
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              updateSlider(undefined, new Date(title).getTime());
+            }}
+          >
+            {content}
+          </span>
+        </Tooltip>
+      );
 
-    return {
-      title: titleView,
-      content: contentView,
+      return {
+        title: titleView,
+        content: contentView,
+      };
     };
+
+    const marks: TimelineItemType[] = [];
+
+    const startMs = startDt
+      ? new Date(startDt).getTime()
+      : new Date(startTimeRef.current).getTime();
+
+    const endMs = endDt
+      ? new Date(endDt).getTime()
+      : new Date(endTimeRef.current).getTime();
+
+    marks.push(markRender(startMs, "开始"));
+
+    let prevTime = startMs;
+
+    tagList.forEach((tag) => {
+      const height = calcTimelineItemHeight(prevTime, tag.dateTime);
+      const tagTime = new Date(tag.dateTime).getTime();
+      if (tagTime >= startMs && tagTime <= endMs) {
+        marks.push({
+          ...markRender(tag.dateTime, tag.label),
+          style: { height, alignContent: "end" },
+        });
+      }
+      prevTime = tagTime;
+    });
+
+    marks.push(markRender(endMs, "结束"));
+
+    setMarks(marks);
+    setDateTimeRange([startMs, endMs]);
+    startTimeRef.current = startMs;
+    endTimeRef.current = endMs;
   };
-
-  const updateSlider = useCallback(
-    (startDt?: number | string, endDt?: number | string) => {
-      const marks: TimelineItemType[] = [];
-
-      const startMs = startDt
-        ? new Date(startDt).getTime()
-        : new Date(dateTimeRange[0]).getTime();
-
-      const endMs = endDt
-        ? new Date(endDt).getTime()
-        : new Date(dateTimeRange[1]).getTime();
-
-      marks.push(markRender(startMs, "开始"));
-
-      let prevTime = startMs;
-
-      tagList.forEach((tag) => {
-        const height = calcTimelineItemHeight(prevTime, tag.dateTime);
-        const tagTime = new Date(tag.dateTime).getTime();
-        if (tagTime >= startMs && tagTime <= endMs) {
-          marks.push({
-            ...markRender(tag.dateTime, tag.label),
-            style: { height, alignContent: "end" },
-          });
-        }
-        prevTime = tagTime;
-      });
-
-      marks.push(markRender(endMs, "结束"));
-
-      setMarks(marks);
-      setDateTimeRange([startMs, endMs]);
-    },
-    [dateTimeRange, markRender, tagList],
-  );
 
   useEffect(() => {
     updateSlider(new Date(startTime).getTime(), new Date(endTime).getTime());
-  }, [startTime, endTime, tagList, updateSlider]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startTime, endTime, tagList]);
 
   return (
     <div className="log-result-select-date">
@@ -98,26 +100,35 @@ function TimeLineView({ startTime, endTime, tagList }: IProps) {
           重置
         </Button>
       </div>
-      <span>{new Date(dateTimeRange[0]).toLocaleString()}</span>
+      <span>
+        <DatePicker
+          showTime
+          allowClear={false}
+          placement="bottomLeft"
+          prefix="开始时间"
+          value={dayjs(dateTimeRange[0])}
+          onOk={(value) => {
+            updateSlider(dayjs(value).valueOf());
+          }}
+        />
+      </span>
       <div className="log-result-select-date-range">
         <div className="log-result-select-date-range-container">
-          {/* <Slider
-                range
-                vertical
-                reverse
-                value={sliderRange}
-                marks={marks}
-                onChange={(value) => {
-                  setSliderRange([value[0], value[1]]);
-                }}
-                onChangeComplete={(value) => {
-                  updateSlider(value[0], value[1]);
-                }}
-              /> */}
           <Timeline mode={"start"} items={marks} />
         </div>
       </div>
-      <span>{new Date(dateTimeRange[1]).toLocaleString()}</span>
+      <span>
+        <DatePicker
+          showTime
+          allowClear={false}
+          placement="topLeft"
+          prefix="结束时间"
+          value={dayjs(dateTimeRange[1])}
+          onOk={(value) => {
+            updateSlider(undefined, dayjs(value).valueOf());
+          }}
+        />
+      </span>
     </div>
   );
 }
